49361a5fbc7e02a500463f1f898f6971a2fbc319
Eric C. Singer <ericcsinger@users.noreply.github.com>
2018-09-15 17:02:14 -0400

    Updated module to v1.1.0
    I'm going to throw an error if a translation fails.  This is probaby the safest thing to do.

---
+++ 1.1.0/Functions/Convert-ECSAccountToSID.ps1
@@ -0,0 +1,100 @@
+﻿Function Convert-ECSAccountToSID
+    {
+    <#
+    .SYNOPSIS
+    Retrieves the SID for a given account name
+
+    .DESCRIPTION
+    Convert-ECSAccountToSID is used to convert an account to a SID
+
+    .PARAMETER AccountName
+    This parameter can be in the format of the following:
+    
+    Name
+    Domain\Name
+    Name@domain.com
+
+    Specifying the domain, will result in the most accurate return.  if there are two accounts named "example".  One local and one domain, the local will return by default.
+
+    .EXAMPLE
+    This is an example of converting a single account.
+
+    Convert-ECSAccountToSID -AccountName "AccountName"
+
+    Convert-ECSAccountToSID -AccountName "Domain\AccountName"
+
+    Convert-ECSAccountToSID -AccountName "AccountName@Domain.com"
+    
+    .EXAMPLE
+    This example uses an array of objects
+
+    $AllAccounts = Get-content c:\AllAccounts.txt
+
+    Convert-ECSAccountToSID -AccountName $AllAccounts
+
+    .EXAMPLE
+    This example uses is from a pipeline
+
+    Get-content c:\AllAccounts.txt | Convert-ECSAccountToSID 
+
+    #>
+    [CmdletBinding()]
+    Param
+	    (
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $True,
+            HelpMessage="Enter an NTAccount in the format of domain\object or simply object (domain\object is the most accurate)"
+            )]
+        [ValidateNotNull()]    
+        $AccountName
+
+        )
+	    
+    ##########################################################################################################
+    #Process block
+
+    process 
+        {
+
+        Foreach ($Object in $AccountName)
+            {
+            Try
+                {
+                Write-Verbose "########################################################"
+                Write-Verbose "Attempting to convert $Object to SID"
+
+                #Creating a user or other object
+                $objUser = New-Object System.Security.Principal.NTAccount($Object)     
+
+                #Translating the object to a SID
+                $strSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
+
+                #Now to have a clean consistent object, we're going to convert the SID back to an NTAccount
+                $NTAccount = Convert-ECSSIDToAccount -SID $($strSID.value)
+                    
+                Write-Verbose "Account lookup succeeded"
+
+                New-Object PSObject -Property @{
+                    NTAccount = $($NTAccount.NTAccount)
+                    SAMAccountName = $($NTAccount.SAMAccountName)
+                    SAMAccountNetBIOSName = $($NTAccount.SAMAccountNetBIOSName)
+                    SID = $($NTAccount.SID)
+                    AccountDomainSID = $($NTAccount.AccountDomainSID)
+                    AccountName = $Object
+                    }
+                }
+            Catch
+                {
+                $Exception = $_.Exception 
+                Write-Verbose "We had an exception looking up the account, see below for the message"
+                Throw "Failed to convert $Object to SID, exception message = $($Exception.Message)"
+                }
+
+            }
+        }
+
+    #End Process block
+    ##########################################################################################################
+
+    }
---
+++ 1.1.0/Functions/Convert-ECSSIDToAccount.ps1
@@ -0,0 +1,98 @@
+﻿Function Convert-ECSSIDToAccount
+    {
+    <#
+    .SYNOPSIS
+    Retrieves the SAMAccountName and NTAccount for a given SID.
+
+    .DESCRIPTION
+    Convert-ECSSIDToAccount is used to convert a SID to an NTAccount or SAMAccountName.  This works for SIDs of domain and local accounts.
+
+    .PARAMETER SID
+    This parameter must be in the format of s-1-5-21-etc.  It accepts pipeline input, multivalue objects and singular SIDS.
+
+    .EXAMPLE
+    This is an example of converting a single SID.
+
+    Convert-ECSSIDToAccount -SID "S-1-5-21-3917423434342070-3434343434-65034343434228590"
+    
+    .EXAMPLE
+    This example uses an array of objects
+
+    $AllSIDS = Get-content c:\AllSIDS.txt
+
+    Convert-ECSSIDToAccount -SID $AllSIDS
+
+    .EXAMPLE
+    This example uses is from a pipeline
+
+    Get-content c:\AllSIDS.txt | Convert-ECSSIDToAccount 
+
+    #>
+    [CmdletBinding()]
+    Param
+	    (
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $True,
+            HelpMessage="Enter a SID in the format of S-1-5-21 to retrive the NTAccount and SAMAccount names"
+            )]
+        [Validatescript(
+            {
+            #Validating if SID is in the correct format
+            $SIDRegexPattern = "S-\d-\d-\d+"
+            If ($_ -match $SIDRegexPattern)
+                {
+                $true
+                }
+            Else
+                {
+                Throw "The SID $SID you entered does not start with S-number-number-number... format needed"
+                }
+            })]
+        $SID
+
+        )
+	    
+    
+    ##########################################################################################################
+    #Process block
+
+    process 
+        {
+
+        Foreach ($Object in $SID)
+            {
+            Try
+                {
+                Write-Verbose "########################################################"
+                Write-Verbose "Attempting to convert $Object to AccountName"
+
+                #Creating a SID object
+                $objSID = New-Object System.Security.Principal.SecurityIdentifier ($Object) 
+
+                #Translating the SID to the AccountName
+                $SIDMapping = $objSID.Translate( [System.Security.Principal.NTAccount]) 
+
+                Write-Verbose "SID lookup succeeded"
+
+                New-Object PSObject -Property @{
+                    NTAccount = $SIDMapping.Value
+                    SAMAccountName = ($SIDMapping.Value -split "\\")[1]
+                    SAMAccountNetBIOSName = ($SIDMapping.Value -split "\\")[0]
+                    SID = $($Object)
+                    AccountDomainSID = $($objSID.AccountDomainSid)
+                    }
+                }
+            Catch
+                {
+                $Exception = $_.Exception 
+                Write-Verbose "We had an exception looking up the SID, see below for the message"
+                Throw "Failed to convert $Object to AccountName, exception message = $($Exception.Message)"
+                }
+            }
+        }
+
+    #End Process block
+    ##########################################################################################################
+
+    }
